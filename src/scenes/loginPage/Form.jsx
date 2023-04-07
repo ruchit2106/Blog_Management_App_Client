ae038d89f0e9007cf090830281554cf85982fb54
ruchit2106 <87220033+ruchit2106@users.noreply.github.com>
2023-04-07 19:11:19 +0530

    Update Form.jsx

--- src/scenes/loginPage/Form.jsx
+++ src/scenes/loginPage/Form.jsx
@@ -104,7 +104,7 @@ const Form = () => {
     }
 
     else {
-      const loggedInResponse = await fetch(`http://${process.env.REACT_APP_IP}:3001/auth/login`, {
+      const loggedInResponse = await fetch(`${process.env.REACT_APP_IP}/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(values),
